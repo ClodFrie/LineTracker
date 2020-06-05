57679c505c05598d85f45b7632c1121f0563f42a
Richard <r.fischereder@gmail.com>
2020-06-05 16:12:02 +0200

    added usb cam

--- pyimagesearch.py
+++ pyimagesearch.py
@@ -17,18 +17,31 @@ args = vars(ap.parse_args())
 vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
 time.sleep(2.0)
 
+# Load a cascade file for detecting faces
+faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+
 # loop over the frames from the video stream
 while True:
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 400 pixels
 	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+	frame = imutils.resize(frame, width=800)
 	# draw the timestamp on the frame
 	timestamp = datetime.datetime.now()
 	ts = timestamp.strftime("%A %d %B %Y %I:%M:%S%p")
 	cv2.putText(frame, ts, (10, frame.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX,
 		0.35, (0, 0, 255), 1)
-	# show the frame
+
+	# Convert into grayscale
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	# # Look for faces in the image using the loaded cascade file
+	faces = faceCascade.detectMultiScale(gray, 1.2, 5)
+    # for (x,y,w,h) in faces:
+    #     # Create rectangle around faces
+    #     cv2.rectangle(frame,(x,y),(x+w,y+h),(255,255,0),2)
+
+	# # show the frame
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 	# if the `q` key was pressed, break from the loop
@@ -39,23 +52,3 @@ cv2.destroyAllWindows()
 vs.stop()
 
 
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
-# allow the camera to warmup
-time.sleep(0.1)
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-	# grab the raw NumPy array representing the image, then initialize the timestamp
-	# and occupied/unoccupied text
-	image = frame.array
-	# show the frame
-	cv2.imshow("Frame", image)
-	key = cv2.waitKey(1) & 0xFF
-	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
